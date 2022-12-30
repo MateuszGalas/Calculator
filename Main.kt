c32c098ed4bb90af2e4899027b0369e74de9de6c
Mateusz Galaś <mateuszgalas94@gmail.com>
2022-12-30 14:02:20 +0100

    Added possibility to assign value to variable that will be remembered.

--- Main.kt
+++ Main.kt
@@ -1,6 +1,7 @@
 package calculator
 
 class Calculator {
+    val variables = mutableMapOf<String, Int>()
 
     fun operation(operator: String): String {
         return when (operator.first()) {
@@ -19,39 +20,95 @@ class Calculator {
     fun subtract(a: Int, b: Int): Int {
         return a - b
     }
+
+    fun variable(variable: String): Boolean {
+        return variables.containsKey(variable)
+    }
+
+    fun variableToDigit(variable: String): Int {
+        return variables[variable]!!
+    }
+
+    fun makeOperations(input: String) {
+        val res = input.split(" ")
+        var result = if (variable(res[0])) variableToDigit(res[0]) else res[0].toInt()
+
+        for (i in 1..res.lastIndex step 2) {
+            when (operation(res[i])) {
+                "+" -> {
+                    result = if (variable(res[i + 1])) {
+                        add(result, variableToDigit(res[i + 1]))
+                    } else {
+                        add(result, res[i + 1].toInt())
+                    }
+                }
+
+                "-" -> {
+                    result = if (variable(res[i + 1])) {
+                        subtract(result, variableToDigit(res[i + 1]))
+                    } else {
+                        subtract(result, res[i + 1].toInt())
+                    }
+                }
+            }
+        }
+        println(result)
+    }
 }
 
 fun main() {
     val calc = Calculator()
 
     while (true) {
-        val input = readln().split(" ")
+        val input = readln().trim()
 
-        if (input.first() == "") continue
-        if (input.first().equals("/exit", true)) println("Bye!").also { return }
-        if (input.first().equals("/help", true)) {
-            println("The program calculates the sum or subtraction of numbers, you can also mix them" +
-                    "like number + number - number. Program works if there are more operators than needed" +
-                    "number +++ number -- number")
-            continue
-        }
-        if (input.first().matches("""/\w+""".toRegex())) {
-            println("Unknown command")
-            continue
-        }
+        when {
+            input == "" -> {}
+            input == "/exit" -> println("Bye!").also { return }
+            input == "/help" -> {
+                println(
+                    "The program calculates the sum or subtraction of numbers, you can also mix them" +
+                            "like number + number - number. Program works if there are more operators than needed" +
+                            "number +++ number -- number"
+                )
+            }
 
-        try {
-            var result = input[0].toInt()
-            for (i in 1..input.lastIndex step 2) {
-                when(calc.operation(input[i])) {
-                    "+" -> result = calc.add(result, input[i + 1].toInt())
-                    "-" -> result = calc.subtract(result, input[i + 1].toInt())
+            input.startsWith("/") -> {
+                println("Unknown command")
+            }
+
+            input.matches("""(\d+|[a-zA-Z]+)\s+(\++|-+)\s+(\d+|[a-zA-Z]+).*""".toRegex()) -> {
+                try {
+                    calc.makeOperations(input)
+                } catch (e: Exception) {
+                    println("Invalid expression")
                 }
             }
-            println(result)
-        } catch (e: Exception) {
-            println("Invalid expression")
-            continue
+
+            input.matches("""[a-zA-Z]+""".toRegex()) -> {
+                if (calc.variable(input)) {
+                    println(calc.variableToDigit(input))
+                } else {
+                    println("Unknown variable")
+                }
+            }
+
+            input.matches(""".*=.*""".toRegex()) -> {
+                val variables = input.split("""(=)""".toRegex()).map { it.trim() }
+                if (!variables.first().matches("""[a-zA-Z]+""".toRegex())) {
+                    println("Invalid identifier")
+                }
+                if (!variables.last().matches("""(\d+|[a-zA-Z]+)""".toRegex()) || variables.size > 2) {
+                    println("Invalid assignment")
+                }
+                if (variables[1].matches("""\d+""".toRegex())) {
+                    calc.variables[variables[0]] = variables[1].toInt()
+                } else if (!calc.variable(variables[1])) {
+                    println("Unknown variable")
+                } else {
+                    calc.variables[variables[0]] = calc.variableToDigit(variables[1])
+                }
+            }
         }
     }
 }
